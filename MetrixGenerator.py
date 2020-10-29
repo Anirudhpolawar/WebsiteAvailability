da879c7e92ca91adbc13a82a2dc7c3427d70f9e8
anirudh polawar <36797874+Anirudhpolawar@users.noreply.github.com>
2020-10-29 15:56:59 +0530

    Update MetrixGenerator.py

--- MetrixGenerator.py
+++ MetrixGenerator.py
@@ -18,15 +18,12 @@ try:
     cursor = con.cursor()
     print (con.get_dsn_parameters())
     SQL = "Insert into allsafe (datetime,statuscode) values (%s,%s)"
-    print("MetrixGenerator  Started : \n")
     print("TimeStamp  \t\t Status Code")
-    while True:
-        response = requests.get('https://allsafe.in/')
-        timenow = dt.now().strftime("%Y-%m-%d %H:%M:%S")
-        cursor.execute(SQL,(timenow,response.status_code))
-        con.commit()
-        print(timenow+" \t "+str(response.status_code))
-        time.sleep(0.500)
+    response = requests.get('https://allsafe.in/')
+    timenow = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+    cursor.execute(SQL,(timenow,response.status_code))
+    con.commit()
+    print(timenow+" \t "+str(response.status_code))
 except (Exception, psycopg2.Error) as error :
     print ("Error : ", error)
 finally:
